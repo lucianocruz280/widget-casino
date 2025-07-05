be4c8e946aebb7991ea989d3c4c456e3ae65d8f4
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-07-05 13:27:20 -0500

    terminar widget

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,12 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import Widget from "@/components/Widget";
 import { Toaster } from "react-hot-toast";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
     <div className="relative w-full h-screen bg-white">
-       <Toaster />
+      <Toaster />
       <Widget />
+      <iframe src="https://dota.click" className="h-full w-full" />
     </div>
   );
 }
